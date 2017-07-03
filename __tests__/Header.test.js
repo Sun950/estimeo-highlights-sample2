74269bc6b19cf4e761375aa6711116298a3d44f4
boulay_a <aboulay950@gmail.com>
2017-07-03 17:03:48 +0200

    update

--- __tests__/Header.test.js
+++ __tests__/Header.test.js
@@ -8,10 +8,26 @@ import Header from '../components/Header';
 
 describe('<Header />', () => {
 
-    /**
-     * TODO
-     */
-    it('will fail', () => {
-        expect(false).toBe(true)
+    it('is equal to Estimeo Hightlights', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('title').text()).toBe('Estimeo Highlights')
     })
+    
+    it('link Roboto is registered', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.contains('<link href=\"https://fonts.googleapis.com/css?family=Roboto\" rel=\"stylesheet\" />'))
+    })
+
+    it('link stylesheet blabla', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.contains('<link rel="stylesheet" href="/static/css/estimeo-style.css"/>'))
+    })
+
+    it('test style only once', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('style').length).toBe(1)
+    })
+    /*it('will fail', () => {
+        expect(false).toBe(true)
+    })*/
 })
--- __tests__/Welcome.test.js
+++ __tests__/Welcome.test.js
@@ -8,10 +8,27 @@ import Welcome from '../components/Welcome';
 
 describe('<Welcome />', () => {
 
-    /**
-     * TODO
-     */
-    it('will success', () => {
-        expect(true).toBe(true)
+    it('only one h1', () => {
+    const wrapper = shallow(<Welcome />);
+    expect(wrapper.find('h1').length).toBe(1)
     })
+
+    it('only one p', () => {
+    const wrapper = shallow(<Welcome />);
+    expect(wrapper.find('p').length).toBe(1)
+    })
+
+    /*it('h1 equals to Welcome!', () => {
+    const wrapper = shallow(<Welcome />);
+    expect(wrapper.find('h1').text()).toBe('Welcome !')
+    })
+
+     it('p equals to MyMessage', () => {
+     const wrapper = shallow(<Welcome />);
+     expect(wrapper.find('').text()).toBe('My Message')
+     })*/
+
+    /*it('will success', () => {
+        expect(true).toBe(true)
+    })*/
 })
